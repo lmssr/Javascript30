c8bb7d40fa4005a5085e711b85a28dd28e996100
Luc Mosser <luc.mosser86@orange.fr>
2019-10-17 10:42:36 +0200

    11th day

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,6 @@
+/* Get our elements */
+const player = document.querySelector('.player');
+const video = document.querySelector('.video');
+/* Build our functions */
+
+/* Hook up the event listeners */
