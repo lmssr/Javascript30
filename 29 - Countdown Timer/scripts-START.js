f14164612f95fa3786aef58505bb8d38be3727c7
Luc Mosser <luc.mosser86@orange.fr>
2019-12-04 21:03:25 +0100

    29th day

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,4 @@
+let countdown;
+let timerDisplay = document.querySelector('.display__time-left');
+let end = document.querySelector('.display__end-time');
+let buttons = document.querySelectorAll('[data-time]');
